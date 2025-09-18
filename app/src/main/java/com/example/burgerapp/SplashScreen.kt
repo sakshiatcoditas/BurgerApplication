a95609bb7829a90bb105828309afb85908676162
Sakshi <sakshi.bhosale@coditas.com>
2025-09-18 10:18:08 +0530

    Updated SplashScreen

--- app/src/main/java/com/example/burgerapp/SplashScreen.kt
+++ app/src/main/java/com/example/burgerapp/SplashScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -34,7 +35,7 @@ fun SplashScreen() {
             contentAlignment = Alignment.Center
         ) {
             Text(
-                text = "Foodgo",
+                text = stringResource(id = R.string.app_name),
                 fontFamily = LobsterFont,
                 fontSize = 60.sp,
                 color = Color.White
