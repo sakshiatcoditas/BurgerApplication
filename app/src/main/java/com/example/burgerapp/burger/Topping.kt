36c75b54f4b9ddce7411a1b41dd725200dad37c0
Sakshi <sakshi.bhosale@coditas.com>
2025-10-05 20:45:22 +0530

    updated

--- app/src/main/java/com/example/burgerapp/burger/Topping.kt
+++
@@ -1,7 +0,0 @@
-package com.example.burgerapp.burger
-
-data class Topping(
-    val name: String,
-    val imageRes: Int,
-    val price: Double = 0.0
-)
--- app/src/main/java/com/example/burgerapp/navigation/Screen.kt
+++ app/src/main/java/com/example/burgerapp/navigation/Screen.kt
@@ -7,13 +7,6 @@ sealed class Screen(val route: String) {
     object ForgotPassword : Screen("forgot_password")
     object Home : Screen("home")
 
-    object BurgerDetail : Screen("burgerDetail/{burgerId}") {
-        fun createRoute(burgerId: String) = "burgerDetail/$burgerId"
-    }
 
-    object Custom : Screen("customScreen/{burgerId}/{portion}/{spiceLevel}") {
-        fun createRoute(burgerId: String, portion: Int, spice: Float) =
-            "customScreen/$burgerId/$portion/$spice"
-    }
 
 }
